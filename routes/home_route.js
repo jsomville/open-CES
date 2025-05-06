6c65df044fc7b96029e7261bcdf82cd6e7aac9e9
jsomville@hotmail.com <jsomville@hotmail.com>
2025-05-06 19:14:42 +0200

    update home page

--- routes/home_route.js
+++ routes/home_route.js
@@ -5,8 +5,6 @@ const router = express.Router();
 import { readFile } from 'fs/promises';
 const pkg = JSON.parse(await readFile(new URL('../package.json', import.meta.url)));
 
-const welcome_text = "Welcome to Open-CES, this application exposes web api to manage exchange using local currency. This solution allows to manage currencies, users, accounts, vouchers and perform operations like top-off account, transfer."
-
 const home_template = () => {
     return `
         <!DOCTYPE html>
@@ -21,13 +19,13 @@ const home_template = () => {
         </head>
         <body>
             <div class="header">
-                <h1>Welcome to Open Currency Exchange System</h1>
-                <h2>${pkg.version}</h2>
+                <h1>Welcome to <b>${pkg.name}</b></h1>
+                <h2>Version : ${pkg.version}</h2>
             </div>
-            <div>
-                <p>${welcome_text}</p>
+            <div class="content">
+                <p>${pkg.description}</p>
             </div>
-            <div>
+            <div class="content">
                  <table>
                     <tr>
                         <th>Component</th>
@@ -43,7 +41,11 @@ const home_template = () => {
                     </tr>
                    
                 </table> 
-
+            </div>
+            <div class="footer">
+                <div>
+                    <div>GitHub : <a href= ${pkg.homepage}> ${pkg.homepage} </a> </Github>
+                </div>
             </div>
         </body>
         </html>`
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 import app from './app.js'
 
 //Server Configurations
-const VERSION = process.version
 const PORT = process.env.PORT || 8000
+
+import { readFile } from 'fs/promises';
+const pkg = JSON.parse(await readFile(new URL('./package.json', import.meta.url)));
 
 //Run the server
 app.listen(PORT, () => {
+
     let environement = "in DEV";
     if (process.env.ENVIRO === "PRD"){
         environement = "in production";
     }
 
-    console.log (`Server is running on port ${PORT} ${environement}`);
+    console.log (`${pkg.name} Server is running on port ${PORT} - ${process.env.ENVIRO} - version: ${pkg.version}`);
 })
 
 export default app;
